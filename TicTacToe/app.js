e7a7d51834fad8d74e586914d3a0dc51ef5d463f
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-05-26 10:31:26 +0530

    further changes

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -1,6 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
-
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 let turnO = true; //playerX,playerO
 const winPatterns = [
   [0, 1, 2],
@@ -31,10 +33,25 @@ boxes.forEach((box) => {
     checkWinner();
   });
 });
+const showWinner = (winner) => {
+  msg.innerText = `Congrats!, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+};
 
 const checkWinner = () => {
   for (let pattern of winPatterns) {
-    console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
-    console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("winner", pos1Val);
+        showWinner(pos1Val);
+      }
+    }
   }
 };
+
+
+const 
