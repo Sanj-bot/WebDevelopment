e22570d75d175effb008a38640890632233261c4
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-08-20 14:45:31 +0530

    update

--- PROJECTS/FETCHAPI/app.js
+++ PROJECTS/FETCHAPI/app.js
@@ -1,7 +1,6 @@
 const URL = "https://cat-fact.herokuapp.com/facts";
 const factPara = document.querySelector("#fact");
 const btn = document.querySelector("#btn");
-
 // const getFacts = async () => {
 //   console.log("getting data.....");
 //   let response = await fetch(URL);
