3f3059d155d6c2e201cec5a5e3556eab01f9f423
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-21 21:13:00 +0530

    update

--- FrontEnd/REACT/Course/m2/01vitereact/src/App.jsx
+++ FrontEnd/REACT/Course/m2/01vitereact/src/App.jsx
@@ -1,5 +1,12 @@
+import Chai from "./chai";
 function App() {
-  return <h1>Chai or react with vite | Sanjay</h1>;
+  return (
+    <>
+    <Chai/>
+    <h1>chai or react</h1>
+    </>
+  
+  );
 }
 
 export default App;
