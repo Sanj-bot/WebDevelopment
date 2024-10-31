2771e3850132d39be7b5e166ade1ae3dff0bb6a7
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-31 11:34:03 +0530

    hooks

--- FrontEnd/REACT/Course/m5/02counter/src/App.jsx
+++ FrontEnd/REACT/Course/m5/02counter/src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-let counter = 1;
+
 function App() {
-  // let counter = 5;
-  // counter++;
+  let [counter, setCounter] = useState(15);
+
   const addValue = () => {
     console.log("value added", Math.random(), counter);
-    counter++;
+    counter = counter + 1;
+    setCounter(counter);
   };
   const removeValue = () => {
     counter--;
+    setCounter(counter);
     console.log("value subtracted", Math.random(), counter);
   };
   return (
