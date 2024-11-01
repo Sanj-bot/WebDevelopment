09b881af7fa8ee2b8ceef42f42aa004b7d10b896
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-01 10:52:21 +0530

    updated

--- FrontEnd/REACT/Course/m7/02tailwind/src/App.jsx
+++ FrontEnd/REACT/Course/m7/02tailwind/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  let myObj = {
+    username: "Sanjay",
+    age: 23,
+  };
+  let newArray = [1, 2, 3, 4, 5];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind test!</h1>
+      <Card channel="chaiorcode" someObje={myObj} array={newArray} />
+      <Card channel="chaiorcode" />
+      <Card username={"Mimo"} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
