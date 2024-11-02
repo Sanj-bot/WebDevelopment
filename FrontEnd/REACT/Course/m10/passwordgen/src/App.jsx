b87f259f54383c8dc2959b7b8aa58ed5eb859bcc
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-02 09:29:22 +0530

    passgen app

---
+++ FrontEnd/REACT/Course/m10/passwordgen/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className="text-4xl text-white text-center">Password generator</h1>
+    </>
+  )
+}
+
+export default App
