444bf85ff087010cb14eeb7fb0930247c7037501
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-03 11:52:29 +0530

    commit

--- FrontEnd/REACT/Course/m11/currencyConverter/src/hooks/useCurrencyInfo.js
+++ FrontEnd/REACT/Course/m11/currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,4 +1,4 @@
-import {useEffect,useState} from "react"react
+import {useEffect,useState} from "react"
 function useCurrencyInfo(currency) {
     useEffect(()=>{
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
