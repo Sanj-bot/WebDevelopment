b066c03f5cfbc3e59c0ea508c1d7275579bc8046
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-12 13:51:35 +0530

    practice

---
+++ FrontEnd/Javascript/m1/p2.js
@@ -0,0 +1,20 @@
+function change() {
+  document.getElementById("food").innerHTML = "Top 4 foods worldwide:";
+}
+
+function a() {
+  let a = "Dal batti";
+  document.getElementById("a").innerHTML = "Dal batti";
+}
+function b() {
+  let b = "alloo paratha";
+  document.getElementById("b").innerHTML = b;
+}
+function c() {
+  let c = "paneer pakoda";
+  document.getElementById("c").innerHTML = c;
+}
+function d() {
+  let d = "chole kulche";
+  document.getElementById("d").innerHTML = d;
+}
