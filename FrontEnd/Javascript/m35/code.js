9a7725d30aa95879d148b37d0c0e57b8a1aec684
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-14 09:48:09 +0530

    update

---
+++ FrontEnd/Javascript/m35/code.js
@@ -0,0 +1,10 @@
+// document.getElementById('owl').onclick=function(){
+//     alert("owl ko ku click kai")
+// }
+
+// document.getElementById('owl').addEventListener('click',function(){
+//     alert("owl clicked again");
+// },false)
+
+
+//attachEvent()
