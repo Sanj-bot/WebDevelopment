cc1410c43f338c2e29f56216e52cd4c4d32fde5a
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-14 08:05:23 +0530

    update

--- FrontEnd/Javascript/m33/p2/script.js
+++ FrontEnd/Javascript/m33/p2/script.js
@@ -3,3 +3,8 @@ console.log(div);
 div.className="main";
 div.id="myId";
 div.id=Math.round(Math.random()*10+1);
+div.setAttribute("title","generated title");
+div.style.backgroundColor="green";
+div.style.padding="12px";
+div.innerText="Chai or code";
+document.body.appendChild(div);
