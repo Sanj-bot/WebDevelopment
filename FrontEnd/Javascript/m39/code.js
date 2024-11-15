297067b03390530486caa1b9737fe328931fb33b
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-15 09:45:49 +0530

    update

--- FrontEnd/Javascript/m39/code.js
+++ FrontEnd/Javascript/m39/code.js
@@ -1,4 +1,11 @@
 const requestUrl = "https://api.github.com/users/Sanj-bot";
 const xhr = new XMLHttpRequest();
 xhr.open('GET',requestUrl);
+xhr.onreadystatechange=function () {
+    console.log(xhr.readyState);
+    if (xhr.readyState===4) {
+        console.log(this.responseText);
+    }
+}
+console.log(xhr.readyState);
 xhr.send();
