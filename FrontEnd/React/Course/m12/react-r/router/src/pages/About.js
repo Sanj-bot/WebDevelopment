e60ffd008e570c52f7b9e6633f1abca03424a562
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-17 14:37:21 +0530

    update

---
+++ FrontEnd/React/Course/m12/react-r/router/src/pages/About.js
@@ -0,0 +1,15 @@
+export default function About(){
+    return (
+        <div className="about">
+            <h2> Welcome to about</h2>
+            <p>lorem10lorem10loremm10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lore
+                m10lorem10lorem10lorem10lorem10
+            </p>
+            <p>lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem
+                10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lore
+                m10lorem10lorem10lorem10lorem10
+            </p>
+        </div>
+           
+    )
+}
---
+++ FrontEnd/React/Course/m12/react-r/router/src/pages/Home.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="home">
+      
+      <h2> Welcome to home</h2>
+      <p>
+        lorem10lorem10loremm10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lore
+        m10lorem10lorem10lorem10lorem10
+      </p>
+      <p>
+        lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem
+        10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lore
+        m10lorem10lorem10lorem10lorem10
+      </p>
+    </div>
+  );
+}
