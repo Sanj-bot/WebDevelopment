d69e13c9bf477f4f6c33348b6366e6fc38ad2ea1
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-17 17:01:42 +0530

    update

--- FrontEnd/Javascript/m40/prom.js
+++ FrontEnd/Javascript/m40/prom.js
@@ -1,11 +1,65 @@
-const promiseOne=new Promise(function(resolve,reject){
- 
-    setTimeout(function(){
-        console.log('Async task is complete');
-    },1000);
- 
-    
+const promiseOne = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task is complete");
+    resolve();
+  }, 1000);
+});
+promiseOne.then(function () {
+  console.log("Promise consumed.");
+});
+
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Chai", email: "chai@example.com" });
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user);
+});
+
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (!error) {
+      resolve({ username: "hitesh", password: "123" });
+    } else {
+      reject("ERROR: JS went wrong");
+    }
+  }, 1000);
+});
+
+async function consumePromiseFive() {
+  const response = await promiseFive;
+  console.log(response);
+}
+consumePromiseFive();
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     // console.log(response);
+//     console.log(data);
+//   } catch (error) {
+//     console.log("E: ", error);
+//   }
+// }
+// getAllUsers();
+
+fetch('https://api.github.com/users/Sanj-bot')
+.then((response)=>{
+    return response.json();
+}).then((data)=>{
+    console.log(data);
 })
-promiseOne.then(function(){
-    console.log("Promise consumed.");
-})
+.catch((error)=>console.log(error))
