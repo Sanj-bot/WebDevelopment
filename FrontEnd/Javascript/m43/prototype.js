789e0e14f0e25c7db74877615f7b0682cd45bab2
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-18 11:17:31 +0530

    update

--- FrontEnd/Javascript/m43/prototype.js
+++ FrontEnd/Javascript/m43/prototype.js
@@ -1,23 +1,54 @@
-const myhero = ["thor", "spiderman"];
+// const myhero = ["thor", "spiderman"];
 
-let heroPower = {
-  thor: "hammer",
-  spiderman: "sling",
+// let heroPower = {
+//   thor: "hammer",
+//   spiderman: "sling",
 
-  getSpiderPower: function () {
-    console.log(`Spidy power is ${this.spiderman}`);
-  },
+//   getSpiderPower: function () {
+//     console.log(`Spidy power is ${this.spiderman}`);
+//   },
+// };
+// Object.prototype.hitesh = function () {
+//   console.log(`hitesh is present in all objects....`);
+// };
+
+// Array.prototype.heyHitesh = function () {
+//   console.log(`Hitesh says hello `);
+// };
+
+// heroPower.hitesh();
+
+// myhero.hitesh();
+// myhero.heyHitesh();
+// heroPower.heyHitesh();
+
+const User = {
+  name: "chai",
+  email: "chai@google.com",
 };
-Object.prototype.hitesh = function () {
-  console.log(`hitesh is present in all objects....`);
+const Teacher = {
+  makeVideo: true,
+};
+const TeachingSupport = {
+  isAvailable: false,
 };
 
-Array.prototype.heyHitesh = function () {
-  console.log(`Hitesh says hello `);
+const TASupport = {
+  makeAssignment: "JS assignment",
+  fullTime: true,
+  __proto__: TeachingSupport,
 };
 
-heroPower.hitesh();
+Teacher.__proto__ = User;
 
-myhero.hitesh();
-myhero.heyHitesh();
-heroPower.heyHitesh();
+//modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher);
+
+let anotherUsername = "chai or code        ";
+String.prototype.trueLength = function () {
+  console.log(`${this}`);
+  console.log(`${this.name}`);
+  console.log(`True length is: ${this.trim().length}`);
+};
+anotherUsername.trueLength();
+"mimo".trueLength();
