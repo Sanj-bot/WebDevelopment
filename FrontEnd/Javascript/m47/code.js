03075a17308a74755c57a2b4025a11b3b3c51946
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-19 16:49:50 +0530

    update

--- FrontEnd/Javascript/m47/code.js
+++ FrontEnd/Javascript/m47/code.js
@@ -20,6 +20,13 @@ Object.defineProperty(chai, "name", {
   writable: false,
   enumerable: false,
 });
+
+
+
+
+
+
+
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
 for (let [key, value] of Object.entries(chai)) {
