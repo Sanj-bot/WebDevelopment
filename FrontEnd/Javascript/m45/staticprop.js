cbf32e98294cf15050aca2693da63e7abe27e1a8
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-19 11:38:52 +0530

    update

--- FrontEnd/Javascript/m45/staticprop.js
+++ FrontEnd/Javascript/m45/staticprop.js
@@ -6,10 +6,22 @@ class User {
   logMe() {
     console.log(`Username: ${this.username}`);
   }
-  createId() {
+  //   static now wont be accessed.
+   createId() {
     return `123`;
   }
 }
 
 const hitesh = new User("hitesh");
 console.log(hitesh.createId());
+
+class Teacher extends User {
+  constructor(username,email){ 
+    super(username);
+    this.email = email;
+  }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com");
+console.log(iphone.createId());
+iphone.logMe();
