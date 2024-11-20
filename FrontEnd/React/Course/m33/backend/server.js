2a92649d5da9b5292d9dc1ff7a7e3ab7713441a0
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-20 10:12:14 +0530

    update

---
+++ FrontEnd/React/Course/m33/backend/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('server is ready');
+});
+
+// get a list of 5 jokes
+app.get('/jokes',(req,res)=>{
+   const jokes=[ {
+        id:0,
+        title: 'A joke',
+        content:' This is a joke'
+
+    },
+    {
+        id:1,
+        title :'A joke',
+        content:' This is a joke'
+    },
+    {
+        id:2,
+        title :'A joke',
+        content:' This is a joke'
+    },
+    {
+        id:3,
+        title :'A joke',
+        content:' This is a joke'
+    },
+    {
+        id:4,
+        title :'A joke',
+        content:' This is a joke'
+    },    
+    {
+        id:5,
+        title :'A joke',
+        content:' This is a joke'
+    }
+]
+ res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
