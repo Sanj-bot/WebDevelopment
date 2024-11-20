f603283257ef10ad90cffee8cc9a4f19624fe31d
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-20 11:51:47 +0530

    update

--- FrontEnd/React/Course/m33/backend/server.js
+++ FrontEnd/React/Course/m33/backend/server.js
@@ -1,45 +1,45 @@
 import express from "express";
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('server is ready');
+app.use(express.static('dist'));
+app.get("/", (req, res) => {
+  res.send("server is ready");
 });
 
 // get a list of 5 jokes
-app.get('/api/jokes',(req,res)=>{
-   const jokes=[ {
-        id:0,
-        title: 'A joke',
-        content:' This is a joke'
-
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 0,
+      title: "A joke",
+      content: " This is a joke",
     },
     {
-        id:1,
-        title :'A joke',
-        content:' This is a joke'
+      id: 1,
+      title: "A joke",
+      content: " This is a joke",
     },
     {
-        id:2,
-        title :'A joke',
-        content:' This is a joke'
+      id: 2,
+      title: "A joke",
+      content: " This is a joke",
     },
     {
-        id:3,
-        title :'A joke',
-        content:' This is a joke'
+      id: 3,
+      title: "A joke",
+      content: " This is a joke",
     },
     {
-        id:4,
-        title :'A joke',
-        content:' This is a joke'
-    },    
+      id: 4,
+      title: "A joke",
+      content: " This is a joke",
+    },
     {
-        id:5,
-        title :'A joke',
-        content:' This is a joke'
-    }
-]
- res.send(jokes);
+      id: 5,
+      title: "A joke",
+      content: " This is a joke",
+    },
+  ];
+  res.send(jokes);
 });
 
 const port = process.env.PORT || 3000;
--- FrontEnd/React/Course/m33/frontend/src/App.jsx
+++ FrontEnd/React/Course/m33/frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  // const [count, setCount] = useState(0)
+ 
   const [jokes, setJokes] = useState([]);
   useEffect(() => {
     axios
@@ -16,19 +14,20 @@ function App() {
       .catch((error) => {
         console.log(error);
       });
-  });
+  },[jokes]);
 
   return (
     <>
       <h1>chai or js</h1>
       <p> JOKES: {jokes.length}</p>
 
-      {jokes.map((joke, index) => {
+      {jokes.map((joke, index) => (
         <div key={joke.id}>
           <h3>{joke.title}</h3>
           <p>{joke.content}</p>
-        </div>;
-      })}
+        </div>
+      ))
+      }
     </>
   );
 }
