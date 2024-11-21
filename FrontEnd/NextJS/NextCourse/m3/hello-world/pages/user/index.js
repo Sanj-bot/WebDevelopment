6623645fbfbcf90060a022603d6ecd99d938eb86
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-21 16:00:47 +0530

    update

--- FrontEnd/NextJS/NextCourse/m3/hello-world/pages/user/index.js
+++ FrontEnd/NextJS/NextCourse/m3/hello-world/pages/user/index.js
@@ -1,10 +1,16 @@
+import { useRouter } from "next/router";
+
 const UserIndex = () => {
-    return (
-      <div>
-        <h1> user page</h1>
-      </div>
-    );
-  };
-  
-  export default UserIndex;
-  
+  return (
+    <div>
+      <h1> This is index page!</h1>
+      <button
+        onClick={(e) => router.push("/user/${router.query.user}/settings")}
+      >
+        open setting page
+      </button>
+    </div>
+  );
+};
+
+export default UserIndex;
