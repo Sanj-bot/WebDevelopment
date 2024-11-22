3c2aa496dd35a30f5e4ba8b9b90ab68999ff3751
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-11-22 10:54:04 +0530

    update

--- FrontEnd/REACT/Course/m33/frontend/src/App.jsx
+++ FrontEnd/REACT/Course/m33/frontend/src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  if (error) {
+    return <h1>Something went wrong!!</h1>;
+  }
 
   return (
     <>
       <h1>hello bois, this is my cat api backend</h1>
+      <h2>Number of products are: {products.length}</h2>
     </>
   );
 }
 
 export default App;
+const customReactQuery = () => {
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get("/api/products");
+        console.log(response.data);
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    })();
+  }, []);
+};
